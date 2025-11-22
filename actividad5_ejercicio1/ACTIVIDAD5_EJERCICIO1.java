4035fd55f852b8ac2e953e108769fc91916560ee
tcastillab <tcastilla@unal.edu.co>
2025-11-22 12:54:27 -0500

    Add files via upload

---
+++ actividad5_ejercicio1/ACTIVIDAD5_EJERCICIO1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad5_ejercicio1;
+
+/**
+ *
+ * @author Asus
+ */
+public class ACTIVIDAD5_EJERCICIO1 {
+
+    public static void main(String[] args) {
+       Principal formulario =new Principal();
+       formulario.setVisible(true);
+    }
+}
